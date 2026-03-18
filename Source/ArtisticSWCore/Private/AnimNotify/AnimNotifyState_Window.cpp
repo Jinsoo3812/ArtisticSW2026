31da8f1e24c92d76d2589132e5184e1c45e9a00a
wonkii <wkddnjswns0368@khu.ac.kr>
2026-03-19 02:11:38 +0900

    Make_ANSWindow

---
+++ Source/ArtisticSWCore/Private/AnimNotify/AnimNotifyState_Window.cpp
@@ -0,0 +1,74 @@
+﻿
+#include "AnimNotify/AnimNotifyState_Window.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "GameFramework/Actor.h"
+
+UAnimNotifyState_Window::UAnimNotifyState_Window()
+{
+	Name = TEXT("Window");
+}
+
+FString UAnimNotifyState_Window::GetNotifyName_Implementation() const
+{
+	return Name;
+}
+
+void UAnimNotifyState_Window::NotifyBegin(
+	USkeletalMeshComponent* MeshComp,
+	UAnimSequenceBase* Animation,
+	float TotalDuration,
+	const FAnimNotifyEventReference& EventReference
+)
+{
+	if (!MeshComp)
+		return;
+
+	AActor* OwnerActor = MeshComp->GetOwner();
+	if (!OwnerActor)
+		return;
+	
+	if (BeginEventTag.IsValid())
+	{
+		FGameplayEventData EventData;
+		EventData.EventTag = BeginEventTag;
+		EventData.Instigator = OwnerActor;
+		EventData.Target = OwnerActor;
+
+		UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(
+			OwnerActor,
+			BeginEventTag,
+			EventData
+		);
+	}
+
+}
+
+void UAnimNotifyState_Window::NotifyEnd(
+	USkeletalMeshComponent* MeshComp,
+	UAnimSequenceBase* Animation,
+	const FAnimNotifyEventReference& EventReference
+)
+{
+	if (!MeshComp)
+		return;
+
+	AActor* OwnerActor = MeshComp->GetOwner();
+	if (!OwnerActor)
+		return;
+
+	if (EndEventTag.IsValid())
+	{
+		FGameplayEventData EventData;
+		EventData.EventTag = EndEventTag;
+		EventData.Instigator = OwnerActor;
+		EventData.Target = OwnerActor;
+
+		UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(
+			OwnerActor,
+			EndEventTag,
+			EventData
+		);
+	}
+}
