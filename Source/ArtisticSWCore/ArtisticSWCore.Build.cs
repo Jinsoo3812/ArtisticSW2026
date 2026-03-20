4f73619d1d69de67b41d7132d5ebde3937353dc8
wonkii <wkddnjswns0368@khu.ac.kr>
2026-03-21 01:08:40 +0900

    Update ArtisticSWCore.Build.cs

--- Source/ArtisticSWCore/ArtisticSWCore.Build.cs
+++ Source/ArtisticSWCore/ArtisticSWCore.Build.cs
@@ -17,7 +17,8 @@ public class ArtisticSWCore: ModuleRules
             // 紐⑤뱢 ???대옒?ㅼ쓽 ?ㅻ뜑 ?뚯씪?먯꽌遺???ъ슜??紐⑤뱢 ?섏〈?깆쓣 異붽?
             "GameplayAbilities",
             "GameplayTags",
-            "GameplayTasks"
+            "GameplayTasks",
+            "GASCore"
         });
 
         PublicIncludePaths.AddRange(new string[] {
@@ -25,7 +26,9 @@ public class ArtisticSWCore: ModuleRules
 			"ArtisticSWCore",
 			"ArtisticSWCore/Public",
             "ArtisticSWCore/Public/Item",
-            "ArtisticSWCore/Public/GameFramework"
+            "ArtisticSWCore/Public/GameFramework",
+            "ArtisticSWCore/Public/GameplayAbility",
+            "ArtisticSWCore/Public/AnimNotify",
         });
     }
 }
