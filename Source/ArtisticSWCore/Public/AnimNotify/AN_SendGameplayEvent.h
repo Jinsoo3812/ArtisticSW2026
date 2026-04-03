eccb8b3a47572670b61f17418617d64da1dc64c5
그림자_군주_조연우 <ckhlight2@gmail.com>
2026-04-04 01:32:05 +0900

    수류탄 투척 몽타주 동기화 및 범용 GAS 노티파이 구현
    Feat: 수류탄 투척 몽타주 동기화 및 범용 GAS 노티파이 구현
    
    - C++ 커스텀 노티파이(`AN_SendGameplayEvent`) 추가: 애니메이션 트랙에서 직접 Gameplay Tag 이벤트 전송 기능 구현
    - `GA_ThrowGrenade` 로직 개선:
      - PlayMontageAndWait 및 WaitGameplayEvent를 활용한 시각-논리 완벽 분리 및 동기화
      - Hold & Release 메커니즘 적용 (클릭 해제 시 `Throw_Action` 섹션 점프)
      - CDO 버그 방지를 위해 State.Aiming 태그를 AddLooseGameplayTag로 수동 제어
    - ArtisticSWCore 모듈에 GAS 종속성(GameplayTags, Abilities, Tasks) 추가

---
+++ Source/ArtisticSWCore/Public/AnimNotify/AN_SendGameplayEvent.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "GameplayTagContainer.h"
+#include "AN_SendGameplayEvent.generated.h"
+
+/**
+ * 어떤 애니메이션에서든 Gameplay Tag를 통해 GAS 이벤트를 발생시키는 범용 노티파이입니다.
+ */
+UCLASS()
+class ARTISTICSWCORE_API UAN_SendGameplayEvent : public UAnimNotify
+{
+	GENERATED_BODY()
+
+public:
+	UAN_SendGameplayEvent();
+
+	// 노티파이가 실행될 때 호출되는 함수
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+
+	// 에디터 타임라인에서 태그 이름이 바로 보이도록 하는 편의성 함수
+	virtual FString GetNotifyName_Implementation() const override;
+
+protected:
+	// 블루프린트(에디터)에서 할당할 이벤트 태그
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "GAS")
+	FGameplayTag EventTag;
+
+};
