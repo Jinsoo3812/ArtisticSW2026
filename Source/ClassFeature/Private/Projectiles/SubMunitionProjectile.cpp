cba3a77a08ee9d1861ee3222d3de3946caa06dae
Kim Kwan Hee <vlvkrhksgml3812@naver.com>
2026-05-14 23:24:49 +0900

    시전자에게도 터지도록 임시 수정
    어떻게 해야 자탄이 집속탄처럼 자연스럽게 바닥에 떨어지고, 설치될까?

--- Source/ClassFeature/Private/Projectiles/SubMunitionProjectile.cpp
+++ Source/ClassFeature/Private/Projectiles/SubMunitionProjectile.cpp
@@ -65,7 +65,7 @@ void ASubMunitionProjectile::LaunchSubMunition(const FVector& LaunchVelocity)
 
 void ASubMunitionProjectile::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (!HasAuthority() || OtherActor == this || OtherActor == GetInstigator())
+	if (!HasAuthority() || OtherActor == this)
 	{
 		return;
 	}
