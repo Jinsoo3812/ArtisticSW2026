7a3b2fa89c48d8320cbc992a9d804eff76fe9860
wonkii <wkddnjswns0368@khu.ac.kr>
2026-05-23 12:04:50 +0900

    Deprecated

--- Source/Enemy/Private/Task/BTT_StartPath.cpp
+++ Source/Enemy/Private/Task/BTT_StartPath.cpp
@@ -28,19 +28,5 @@ EBTNodeResult::Type UBTT_StartPath::ExecuteTask(UBehaviorTreeComponent& OwnerCom
 	{
 		return EBTNodeResult::Failed;
 	}
-
-	UPathMovement* PathMovement = Enemy->GetPathMovementComponent();
-	if (!PathMovement || !PathMovement->GetCurrentPath() || PathMovement->HasReachedGoal())
-	{
-		return EBTNodeResult::Failed;
-	}
-
-	// [추가된 방어 코드] 이미 경로 이동 중이라면 굳이 다시 켤 필요 없이 즉시 성공 처리
-	if (PathMovement->IsPathMovementActive())
-	{
-		return EBTNodeResult::Succeeded;
-	}
-
-	PathMovement->StartPathMovement();
 	return EBTNodeResult::Succeeded;
 }
