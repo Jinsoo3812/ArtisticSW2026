9f75d57d31ba7133aed755f14dc2d745742f817b
조연우 <ckhlight2@gmail.com>
2026-06-29 18:47:49 +0900

    Start 도중 방향전환 시 버그 해결 (원격)

--- Source/ClassFeature/Private/Animation/MotionMatchingAnimInstance.cpp
+++ Source/ClassFeature/Private/Animation/MotionMatchingAnimInstance.cpp
@@ -957,7 +957,7 @@ void FMotionMatchingAnimInstanceProxy::UpdateAnimationNode_WithRoot(const FAnima
     const bool bStabilizeRemoteTransition =
         UpdatedPawn &&
         UpdatedPawn->GetLocalRole() == ROLE_SimulatedProxy &&
-        IsTransitionMotionMatchingState(UpdatedMotionState);
+        (UpdatedMotionState == ELocomotionState::Stop || UpdatedMotionState == ELocomotionState::Landing);
     if (bStabilizeRemoteTransition)
     {
         for (FCachedMotionMatchingNodeInfo& Info : CachedMMNodes)
