f13c4165d22547dadd1f70a13f40f740d2e11884
Kim Kwan Hee <vlvkrhksgml3812@naver.com>
2026-08-19 21:24:56 +0900

    Compute Shader 적용으로 프레임 10% 정도 오른듯?

--- Source/WaterAndShip/Private/WaterAndShipModule.cpp
+++ Source/WaterAndShip/Private/WaterAndShipModule.cpp
@@ -1,3 +1,20 @@
-﻿#include "Modules/ModuleManager.h"
-	 
-IMPLEMENT_MODULE(FDefaultModuleImpl, WaterAndShip);
+#include "Modules/ModuleManager.h"
+#include "Misc/Paths.h"
+#include "ShaderCore.h"
+
+class FWaterAndShipModule : public IModuleInterface
+{
+public:
+	virtual void StartupModule() override
+	{
+		const FString ShaderDir = FPaths::Combine(FPaths::ProjectDir(), TEXT("Shaders"));
+		AddShaderSourceDirectoryMapping(TEXT("/Project/Shaders"), ShaderDir);
+	}
+
+	virtual void ShutdownModule() override
+	{
+		ResetAllShaderSourceDirectoryMappings();
+	}
+};
+
+IMPLEMENT_MODULE(FWaterAndShipModule, WaterAndShip);
